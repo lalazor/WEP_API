efd7ff19fa0acadb19919a6abb51604308ee24ff
lalazor <Sziszi610@gmail.com>
2025-04-17 15:11:30 +0200

    Db connection and migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using CompleteGuideToAspNetCoreWebApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace complete_guide_to_aspnetcore_web_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        // Define your DbSets here
+        // Example:
+        // public DbSet<EntityName> EntityNames { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using complete_guide_to_aspnetcore_web_api.Data;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
+// Add services to the container.
+//aaaaaa
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "My API", Version = "v2" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
